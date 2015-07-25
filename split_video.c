69134d85ac354bfbab6bd3983ba0a622a2e90155
Kevin Squire <kevin@secondspectrum.com>
2015-07-25 15:30:40 -0700

    Add name to copyright at the top of the file.

--- split_video.c
+++ split_video.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2001 Fabrice Bellard
+ * Copyright (c) 2015 Kevin Squire
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
